0745dd019dbf6a18d2e11c9e0860e00923f2e375
Hasi6 <hasitha.chandula@gmail.com>
2022-08-01 19:53:24 +0530

    updated

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import "dotenv/config";
 import express, { Application, json } from "express";
 import cors from "cors";
 import registerRoutes from "./routes";
+import { NotFoundError } from "./utils/execptions";
+import { errorHandler } from "./middlewares/error-handler";
 
 const app: Application = express();
 
@@ -11,6 +13,12 @@ const PORT: string = process.env.PORT || "5000";
 app.use(json());
 app.use(cors());
 registerRoutes(app);
+
+app.all("*", async () => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`server started at PORT: ${PORT}`);
