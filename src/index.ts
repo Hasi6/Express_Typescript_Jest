969d590eabf3fff68d5352c8590c7039c11dc868
Hasi6 <hasitha.chandula@gmail.com>
2022-08-01 18:58:57 +0530

    add eslint and prettier

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
-import "express-async-errors";
-import "dotenv/config";
-import express, { Application, json } from "express";
-import cors from "cors";
-import registerRoutes from "./routes";
+import 'express-async-errors';
+import 'dotenv/config';
+import express, { Application, json } from 'express';
+import cors from 'cors';
+import registerRoutes from './routes';
 
 const app: Application = express();
 
-const PORT: string = process.env.PORT || "5000";
+const PORT: string = process.env.PORT || '5000';
 
 app.use(json());
 app.use(cors());
